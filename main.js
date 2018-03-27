ac9f0c836b06e3a124adf2a0a4a08e642c1b8694
Rita Patrick <rita.patrick1007@gmail.com>
2018-03-26 21:17:36 -0500

    xhr working

--- main.js
+++ main.js
@@ -1,1 +1,34 @@
 console.log("hi!");
+
+const printToDomString = (domString, divId) => {
+  document.getElementById(divId).innerHTML = domString;
+};
+
+const buildDomString = (fancyArray) => {
+  let domString = "";
+  fancyArray.forEach ((animal) =>{
+    domString += `<h1>${animal.name}</h1>`;
+  })
+  printToDomString(domString, "zoo");
+};
+
+
+function executeThisCodeIfXHRFails () {
+
+
+}
+
+function executeThisCodeAfterFileLoaded () {
+  const data = JSON.parse(this.responseText);
+  buildDomString(data.animals);
+}
+
+const startApplication = () => {
+  let myRequest = new XMLHttpRequest();
+  myRequest.addEventListener("load", executeThisCodeAfterFileLoaded);
+  myRequest.addEventListener("error", executeThisCodeIfXHRFails);
+  myRequest.open("GET","animals.json");
+  myRequest.send();
+};
+
+startApplication ();
